2ed6c9b456947a6de66221720e857207aa9867fd
Esko Evtyukov (School laptop) <eskoonwheels@gmail.com>
2018-03-29 12:22:40 +0300

    Progress on player scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerEntity))]
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private PlayerEntity player;
+
+    private Vector3 inputVector;
+    private Vector3 mousePositionInWorld;
+
+    public float moveSpeed;
+    public float turnSpeed;
+
+    private void Awake()
+    {
+        player = GetComponent<PlayerEntity>();
+    }
+
+    private void Update()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+        mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        player.Move(inputVector, moveSpeed);
+        player.TurnToward(mousePositionInWorld, turnSpeed);
+    }
 }
--- Assets/Scripts/PlayerEntity.cs
+++ Assets/Scripts/PlayerEntity.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerEntity : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector3 input, float moveSpeed)
+    {
+        rb.MovePosition(transform.position + (input.normalized * moveSpeed * Time.deltaTime));
+    }
+
+    public void TurnToward(Vector3 mousePosition, float turnSpeed)
+    {
+
+    }
+
 }
